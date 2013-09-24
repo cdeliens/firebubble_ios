7c75fc7d329e20d900d228555a83ea6e67e0453d
Jose Medrano <josemedrano@Joses-MacBook-Pro.local>
2013-09-24 14:30:49 -0600

    Initial Commit

---
+++ FireBubble/FBViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBViewController.h
+//  FireBubble
+//
+//  Created by Jose Medrano on 9/24/13.
+//  Copyright (c) 2013 Jose Medrano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBViewController : UIViewController
+
+@end
---
+++ FireBubble/FBViewController.m
@@ -0,0 +1,29 @@
+//
+//  FBViewController.m
+//  FireBubble
+//
+//  Created by Jose Medrano on 9/24/13.
+//  Copyright (c) 2013 Jose Medrano. All rights reserved.
+//
+
+#import "FBViewController.h"
+
+@interface FBViewController ()
+
+@end
+
+@implementation FBViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
